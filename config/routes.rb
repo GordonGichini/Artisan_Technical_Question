570037ac7a2899d1f14471a459b57b01f7e622b6
GordonGichini <gordongichini1@gmail.com>
2025-02-12 12:05:39 +0300

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'books#index'
+
+  # Books: list and show 
+  resources :books, only: [:index, :show] do
+    # Route for borrowing a book
+    post 'borrow', on: :member, to: 'borrowings#create'
+  end
+
+  # Borrowings: only the return action is needed
+  resources :borrowings, only: [] do
+    patch 'return', on: :member, to: 'borrowings#update'
+  end
+
+  # Users for sign up and profile page
+  resources :users, only: [:new, :create, :show]
+
+  # Simple session routes for login/logout
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
